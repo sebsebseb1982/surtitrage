eef1c8cddfbc53dda3a66eb84025b6ecf00d025a
Seb <titi@toto.fr>
2020-02-16 16:07:18 +0100

    init

--- src/surtitrage-routes.ts
+++ src/surtitrage-routes.ts
@@ -10,6 +10,9 @@ export class SurtitrageRoutes implements IRoutable {
     repliques: string[];
     fichier: string = 'test/repliques.txt';
     port;
+    positionTop: number = 0;
+    positionLeft: number = 0;
+    fontSize: number = 30;
 
     constructor() {
         this.repliques = fs.readFileSync(this.fichier, 'utf8').split('\n');
@@ -23,6 +26,9 @@ export class SurtitrageRoutes implements IRoutable {
                 (req: Request, res: Response) => {
                     let repliqueMiseEnForme = '';
                     let replique = this.repliques[req.query.replique ? req.query.replique : 0];
+                    this.positionTop = req.query.top ? req.query.top : 0;
+                    this.positionLeft = req.query.left ? req.query.left : 0;
+                    this.fontSize = req.query.fontSize ? req.query.fontSize : 30;
                     _.forEach(replique.split('\\n'), (ligneDansLaReplique) => {
                         repliqueMiseEnForme += `<p>- ${ligneDansLaReplique}</p>`;
                     });
@@ -34,22 +40,46 @@ export class SurtitrageRoutes implements IRoutable {
                                     body { 
                                         background-color: black;
                                         color: white; 
-                                        font-size: 30px;
                                         font-family: Arial, Helvetica, sans-serif;
+                                    }
+                                    #repliques {
+                                        position: relative;
+                                        display: inline-block;
                                     }
                                 </style>
                             </head>
-                            <body>
+                            <body style="font-size: ${this.fontSize}px">
                                 <script>
                                     let showBorders = false;
                                     document.body.onkeyup = function(e){
                                         const urlParams = new URLSearchParams(window.location.search);
                                         const replique = urlParams.get('replique') ? urlParams.get('replique') : 0; 
-                                        if(e.key === 'ArrowRight' && replique < ${this.repliques.length - 1}){
-                                            window.location.href = 'http://localhost:${this.port}/surtitrage?replique=' + new String(parseInt(replique)+1);
+                                        let top = document.getElementById("repliques").style.top ? parseInt(document.getElementById("repliques").style.top, 10) : 0;
+                                        let left = document.getElementById("repliques").style.left ? parseInt(document.getElementById("repliques").style.left, 10) : 0;
+                                        let fontSize = document.getElementsByTagName("body")[0].style.fontSize ? parseInt(document.getElementsByTagName("body")[0].style.fontSize, 10) : 30;
+                                        if(e.key === 'p' && replique < ${this.repliques.length - 1}){
+                                            window.location.href = 'http://localhost:${this.port}/surtitrage?replique=' + new String(parseInt(replique)+1) + '&top=' + top + '&left=' + left + '&fontSize=' + fontSize;
                                         }
-                                        else if(e.key === 'ArrowLeft' && replique > 0){
-                                            window.location.href = 'http://localhost:${this.port}/surtitrage?replique=' + new String(parseInt(replique)-1);
+                                        else if(e.key === 'o' && replique > 0){
+                                            window.location.href = 'http://localhost:${this.port}/surtitrage?replique=' + new String(parseInt(replique)-1) + '&top=' + top + '&left=' + left + '&fontSize=' + fontSize;
+                                        }
+                                        else if(e.key === 'e'){
+                                            document.getElementsByTagName("body")[0].style.fontSize = fontSize - 1;
+                                        }
+                                        else if(e.key === 'r'){
+                                            document.getElementsByTagName("body")[0].style.fontSize = fontSize + 1;
+                                        }
+                                        else if(e.key === 'ArrowUp'){
+                                            document.getElementById("repliques").style.top = top - 10;
+                                        }
+                                        else if(e.key === 'ArrowDown'){
+                                            document.getElementById("repliques").style.top = top + 10;
+                                        }
+                                        else if(e.key === 'ArrowLeft'){
+                                            document.getElementById("repliques").style.left = left - 10;
+                                        }
+                                        else if(e.key === 'ArrowRight'){
+                                            document.getElementById("repliques").style.left = left + 10;
                                         }
                                         else if(e.key === 'b'){
                                             if(showBorders == true) {
@@ -61,7 +91,9 @@ export class SurtitrageRoutes implements IRoutable {
                                         }
                                     }
                                 </script>
-                                ${repliqueMiseEnForme}
+                                <div id="repliques" style="top: ${this.positionTop}px; left: ${this.positionLeft}px">
+                                    ${repliqueMiseEnForme}
+                                </div>
                             </body>
                         </html>
                     `
